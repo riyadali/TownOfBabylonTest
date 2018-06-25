2a60b4e214eb70a57d50fd4c8280535141cf643d
riyadali <riyadali@gmail.com>
2018-06-25 14:16:47 -0400

    Create main-menu.component.ts

---
+++ src/app/main-menu/main-menu.component.ts
@@ -0,0 +1,13 @@
+@Component({
+  selector: 'app-main-menu',
+  templateUrl: './main-menu.component.html',
+  styleUrls: ['./main-menu.component.css']
+})
+export class MainMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
