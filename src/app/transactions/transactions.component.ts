d8d74318fe8087c1c19f58be323c695f8a1979fc
riyadali <riyadali@gmail.com>
2018-09-15 12:22:55 -0400

    Update transactions.component.ts

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -8,7 +8,7 @@ import { TransactionService }  from '../transaction.service';
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
-  styleUrls: [ './transactions.component.css' ]
+  styleUrls: [ './transactions.component.scss' ]
 })
 export class TransactionsComponent implements OnInit {
   transactions: Transaction[];
